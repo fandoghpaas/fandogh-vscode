99bc614fc44f0b9c2884f253eb4c1867c3d05190
Reza Haidari <shoosh2007@gmail.com>
2018-07-17 19:12:56 +0430

    handle authentication error APIs

--- src/client.js
+++ src/client.js
@@ -1,5 +1,6 @@
 const vscode = require('vscode');
 const fandogh = require('fandogh')
+const ws = vscode.workspace.workspaceFolders
 
 class Client {
 
@@ -18,14 +19,17 @@ class Client {
       await context.globalState.update('fandogh.'+state.name, state.value)
     }))
   }
-
   async login(context) {
     try {
-      let username = await vscode.window.showInputBox({placeHolder: 'Username'})
-      let password = await vscode.window.showInputBox({placeHolder: 'Password', password: true})
-      let user = await fandogh.login({username, password})
+      let global_username = await context.globalState.get('fandogh.username')
+      let global_password = await context.globalState.get('fandogh.password')
+      let username = global_username || await vscode.window.showInputBox({placeHolder: 'Username'})
+      let password = global_password || await vscode.window.showInputBox({placeHolder: 'Password', password: true})
+      let user = await fandogh.login({username: username, password: password})
       vscode.window.showInformationMessage('Logged in successfully');
       await context.globalState.update('fandogh.token', user.token)
+      await context.globalState.update('fandogh.username', username)
+      await context.globalState.update('fandogh.password', password)
       return user.token
     } catch(e){
         vscode.window.showErrorMessage(e.message)
@@ -41,16 +45,26 @@ class Client {
       await Client.updateState([{name:'image', value: name}], context)
       return image
     } catch(e){
-        vscode.window.showErrorMessage(e.message)
-        Promise.reject(e)
+        if(e.code === 401){
+          vscode.window.showErrorMessage(e.error.message);
+          await new Client().login(context)
+          await new Client().createImage(context)
+        } else {
+          let error = e.error
+          if(typeof error === 'string'){
+            vscode.window.showErrorMessage(error)
+          } else {
+            if(error['name']){
+              vscode.window.showErrorMessage(error['name'][0])
+            }
+          }
+          Promise.reject(e)
+        }
     }
   }
   async createVersion(context){
     try {
-
       let token = await Client.init(context)
-      let ws = vscode.workspace.workspaceFolders
-      
       let imageName =  await context.globalState.get('fandogh.image')
       let name = await vscode.window.showInputBox({placeHolder: 'Enter image name', value: imageName || ''})
       let version  = await vscode.window.showInputBox({placeHolder: 'Enter image version'})
@@ -59,12 +73,20 @@ class Client {
       await Client.updateState([{name:'image', value: name}, {name: 'version', value: version}], context)
       return version_
     } catch(e){
-      if(e.message){
-        vscode.window.showErrorMessage(e.message)
-      } else {
-        vscode.window.showErrorMessage(e)
+      if(e.code === 401){
+        vscode.window.showErrorMessage(e.error.message);
+        await new Client().login(context)
+        await new Client().createVersion(context)
       }
-        Promise.reject(e)
+      else {
+        let error = e.error
+        if(e.message){
+          vscode.window.showErrorMessage(error.message)
+        } else {
+          vscode.window.showErrorMessage(error)
+        }
+          Promise.reject(e)
+      }
     }
   }
   async createService (context){
@@ -87,12 +109,20 @@ class Client {
       return service
       
     } catch(e){
-      if(e.message){
-        vscode.window.showErrorMessage(e.message)
-      } else {
-        vscode.window.showErrorMessage(e)
+      if(e.code === 401){
+        vscode.window.showErrorMessage(e.error.message);
+        await new Client().login(context)
+        await new Client().createService(context)
       }
-        Promise.reject(e)
+      else {
+        let error = e.error
+        if(e.message){
+          vscode.window.showErrorMessage(error.message)
+        } else {
+          vscode.window.showErrorMessage(error)
+        }
+          Promise.reject(e)
+      }
     }
   }
 
